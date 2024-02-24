9045c4ebc01b20276b21bfd7184bd75fc42b1f9c
GabiJoita <GabiJoita191@gmail.com>
2024-02-24 14:20:46 +0200

    images folder deleted

--- link_manager.py
+++ link_manager.py
@@ -3,6 +3,7 @@ import tkinter as tk
 import webbrowser
 from PIL import ImageTk
 import os
+from pathlib import Path
 
 
 def open_you_tube():
@@ -12,7 +13,7 @@ def open_you_tube():
 
 def open_folder():
     """create a function that open a specify folder"""
-    folder_path = "C:\\Users\\Joita\\..."
+    folder_path = Path(__file__).parent
     os.startfile(folder_path)
 
 
@@ -37,11 +38,11 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("800x500")
     root.title("my app")
-    root.iconbitmap('C:\\Users\\Joita\\...')
+    root.iconbitmap('C:\\Users\\Joita\\Desktop\\it_school\\project\\bot_project\\images\\logo.ico')
     root.configure(bg='#0F5B92')
 
     my_image = ImageTk.PhotoImage(
-        file='C:\\Users\\Joita\\...')
+        file= Path(__file__).parent / 'logo_play.png')
     image_label = tk.Label(root, image=my_image, width=150, height=150)
     image_label.image = my_image
     image_label.grid(row=4, column=5, padx=200, pady=20)
@@ -54,20 +55,22 @@ if __name__ == "__main__":
                          width=30, command=open_folder)
     button_2.grid(row=2, column=1, padx=10, pady=10)
 
-    button_2 = tk.Button(root, text='Google', font=('Arial Bolt', 10), 
+    button_3 = tk.Button(root, text='Google', font=('Arial Bolt', 10), 
                          width=30, command=open_google)
-    button_2.grid(row=4, column=1, padx=10, pady=10)
+    button_3.grid(row=4, column=1, padx=10, pady=10)
 
-    button_2 = tk.Button(root, text='Netflix', font=('Arial Bolt', 10), 
+    button_4 = tk.Button(root, text='Netflix', font=('Arial Bolt', 10), 
                          width=30, command=open_netflix)
-    button_2.grid(row=6, column=1, padx=10, pady=10)
+    button_4.grid(row=6, column=1, padx=10, pady=10)
 
-    button_2 = tk.Button(root, text='Instagram', font=('Arial Bolt', 10), 
+    button_5 = tk.Button(root, text='Instagram', font=('Arial Bolt', 10), 
                          width=30, command=open_instagram)
-    button_2.grid(row=8, column=1, padx=10, pady=10)
+    button_5.grid(row=8, column=1, padx=10, pady=10)
 
     presentation_label = tk.Label(root, text='choose your link', 
                                   font=('Arial Bolt', 16), bg='#0F5B92')
     presentation_label.grid(row=10, column=1, padx=10, pady=10)
+
+    image_label.grid_propagate(False)
 
     root.mainloop()
